83b6c25f26d9c72a613485956d66eae81ee2e395
Eldad Uzman <eldadu1985@gmail.com>
2024-02-27 10:03:39 +0200

    mean implemented

--- tests/test_calculations.cpp
+++ tests/test_calculations.cpp
@@ -16,11 +16,11 @@ class PerformanceTestBase : public ::testing::Test
 protected:
     std::mutex mtx;
     std::chrono::high_resolution_clock::time_point start_time;
-    std::vector<std::chrono::nanoseconds> thread_durations;
+    std::vector<float> thread_durations;
     int num_iterations = 100;
     int num_threads = 4;
     float median_duration_upper_threshold_milliseconds = 1000.0;
-
+    
     virtual void ExecuteTestLogic() = 0;
     void SetUp() override
     {
@@ -35,8 +35,8 @@ protected:
     {
         float percentile_index = float(100 - percentile) / 100.0;
         size_t sorted_array_index = thread_durations.size() * percentile_index;
-        std::chrono::nanoseconds indexed_duration_cron = thread_durations[sorted_array_index];
-        float indexed_duration_milliseconds = (float)(indexed_duration_cron.count() / 1000000);
+        float indexed_duration_cron = thread_durations[sorted_array_index];
+        float indexed_duration_milliseconds = (float)(indexed_duration_cron / 1000000);
         return indexed_duration_milliseconds;
     }
 
@@ -52,7 +52,7 @@ protected:
         }
 
         auto const count = static_cast<float>(thread_durations.size());
-        return std::accumulate(thread_durations.begin(), thread_durations.end(), 0.0) / count;
+        return (std::accumulate(thread_durations.begin(), thread_durations.end(), 0.0) / count) / 1000000;
     }
 
     void RunTest()
@@ -65,7 +65,7 @@ protected:
         auto duration_ns = end_time - start_time;
 
         std::lock_guard<std::mutex> lock(mtx);
-        thread_durations.push_back(duration_ns);
+        thread_durations.push_back(duration_ns.count());
     }
     void RunMultiThreadedTest()
     {
@@ -75,7 +75,7 @@ protected:
             threads.emplace_back([this]
                                  {
                 for (int j = 0; j < num_iterations; ++j) {
-                    RunTest(); // Execute the test logic
+                    RunTest();
                 } });
         }
 
@@ -85,8 +85,12 @@ protected:
         }
 
         std::cout
+            << "Mean duration: " << calculate_average_duration_milliseconds() << " milliseconds" << std::endl
+            << "90th percentile: " << calculate_duration_percentile(10) << " milliseconds" << std::endl
             << "Median duration: " << calculate_median_duration_milliseconds() << " milliseconds" << std::endl
-            << "90th percentile: " << calculate_duration_percentile(90) << " milliseconds" << std::endl;
+            << "90th percentile: " << calculate_duration_percentile(90) << " milliseconds" << std::endl
+            << "95th percentile: " << calculate_duration_percentile(95) << " milliseconds" << std::endl
+            << "99th percentile: " << calculate_duration_percentile(99) << " milliseconds" << std::endl;
         EXPECT_LE(calculate_median_duration_milliseconds(), median_duration_upper_threshold_milliseconds);
     }
 };
