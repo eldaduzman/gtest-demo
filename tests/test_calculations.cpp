1aec0dd084b02662ed6468c5873589d1aa52b1df
Eldad Uzman <eldadu1985@gmail.com>
2024-02-27 09:56:53 +0200

    before mean

--- tests/test_calculations.cpp
+++ tests/test_calculations.cpp
@@ -4,6 +4,8 @@
 #include <mutex>
 #include <vector>
 #include <algorithm>
+#include <numeric>
+
 extern "C"
 {
 #include "../modules/calculations.h"
@@ -16,27 +18,43 @@ protected:
     std::chrono::high_resolution_clock::time_point start_time;
     std::vector<std::chrono::nanoseconds> thread_durations;
     int num_iterations = 100;
+    int num_threads = 4;
+    float median_duration_upper_threshold_milliseconds = 1000.0;
 
     virtual void ExecuteTestLogic() = 0;
     void SetUp() override
     {
         start_time = std::chrono::high_resolution_clock::now();
-        num_iterations = 100;
     }
 
     void TearDown() override
     {
         std::sort(thread_durations.begin(), thread_durations.end());
-        size_t mid_index = thread_durations.size() / 2;
-        std::chrono::nanoseconds median_duration_cron = thread_durations[mid_index];
-        if (thread_durations.size() % 2 == 1)
-        {
-            median_duration_cron = (thread_durations[mid_index - 1] + thread_durations[mid_index + 1]) / 2;
-        }
-        long long median_duration = median_duration_cron.count() / 1000000;
-        std::cout << "Median thread execution duration: " << median_duration << " milliseconds" << std::endl;
-        EXPECT_LE(median_duration, 1000);
     }
+    float calculate_duration_percentile(int percentile)
+    {
+        float percentile_index = float(100 - percentile) / 100.0;
+        size_t sorted_array_index = thread_durations.size() * percentile_index;
+        std::chrono::nanoseconds indexed_duration_cron = thread_durations[sorted_array_index];
+        float indexed_duration_milliseconds = (float)(indexed_duration_cron.count() / 1000000);
+        return indexed_duration_milliseconds;
+    }
+
+    float calculate_median_duration_milliseconds()
+    {
+        return calculate_duration_percentile(50);
+    }
+    float calculate_average_duration_milliseconds()
+    {
+        if (thread_durations.empty())
+        {
+            return 0;
+        }
+
+        auto const count = static_cast<float>(thread_durations.size());
+        return std::accumulate(thread_durations.begin(), thread_durations.end(), 0.0) / count;
+    }
+
     void RunTest()
     {
         auto start_time = std::chrono::high_resolution_clock::now();
@@ -48,6 +66,28 @@ protected:
 
         std::lock_guard<std::mutex> lock(mtx);
         thread_durations.push_back(duration_ns);
+    }
+    void RunMultiThreadedTest()
+    {
+        std::vector<std::thread> threads;
+        for (int i = 0; i < num_threads; ++i)
+        {
+            threads.emplace_back([this]
+                                 {
+                for (int j = 0; j < num_iterations; ++j) {
+                    RunTest(); // Execute the test logic
+                } });
+        }
+
+        for (auto &thread : threads)
+        {
+            thread.join();
+        }
+
+        std::cout
+            << "Median duration: " << calculate_median_duration_milliseconds() << " milliseconds" << std::endl
+            << "90th percentile: " << calculate_duration_percentile(90) << " milliseconds" << std::endl;
+        EXPECT_LE(calculate_median_duration_milliseconds(), median_duration_upper_threshold_milliseconds);
     }
 };
 class TestHappyFlow : public PerformanceTestBase
@@ -62,36 +102,18 @@ protected:
         std::this_thread::sleep_for(std::chrono::milliseconds(10));
     }
 };
-// class TestEmptyArr : public PerformanceTestBase
-// {
-// protected:
-//     void ExecuteTestLogic() override
-//     {
-//         int arr[] = {};
-//         size_t n = sizeof(arr) / sizeof(arr[0]);
-//         float mean = calculate_mean(arr, n);
-//         EXPECT_FLOAT_EQ(mean, 5.5);
-//         std::this_thread::sleep_for(std::chrono::milliseconds(10));
-//     }
-// };
+class TestEmptyArr : public PerformanceTestBase
+{
+protected:
+    void ExecuteTestLogic() override
+    {
+        int arr[] = {};
+        float mean = calculate_mean(arr, 0);
+        EXPECT_FLOAT_EQ(mean, 0);
+    }
+};
 
 TEST_F(TestHappyFlow, Test_Happy_Flow)
 {
-    const int num_threads = 4;
-    // Create and start threads
-    std::vector<std::thread> threads;
-    for (int i = 0; i < num_threads; ++i)
-    {
-        threads.emplace_back([this]
-                             {
-      for (int j = 0; j < num_iterations; ++j) {
-        RunTest(); // Execute the test logic
-      } });
-    }
-
-    // Wait for all threads to finish
-    for (auto &thread : threads)
-    {
-        thread.join();
-    }
+    RunMultiThreadedTest(); // This replaces the previous threading logic
 }
